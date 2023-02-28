6ba4d2a52776476cc73de34424adbb7f8861bae8
Lasseignejk <jaye914@hotmail.com>
2023-02-28 12:40:09 -0500

    Updates to userAccount
    Added a form skeleton, working on getting state to work

--- Avocado/src/components/pages/UserAcct.jsx
+++ Avocado/src/components/pages/UserAcct.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { useState } from "react";
 
 const UserAcct = () => {
-  return <div>UserAcct</div>;
+	const [accountDetails, setAccountDetails] = useState({});
+
+	const setFormState = (e) => {
+		setAccountDetails({
+			...accountDetails,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	return (
+		<div>
+			<form>
+				<h1>User Account</h1>
+				<div>
+					<label htmlFor="name">First Name</label>
+					<input
+						type="text"
+						id="name"
+						name="CustomerFirstName"
+						onChange={(e) => setFormState(e)}
+						value={
+							accountDetails.CustomerFirstName
+								? accountDetails.CustomerFirstName
+								: ""
+						}
+					/>
+				</div>
+				<div>
+					<label htmlFor="name">First Name</label>
+					<input
+						type="text"
+						id="name"
+						name="CustomerLastName"
+						onChange={(e) => setFormState(e)}
+						value={
+							accountDetails.CustomerLastName
+								? accountDetails.CustomerLastName
+								: ""
+						}
+					/>
+				</div>
+				<div>
+					<button type="button">SUBMIT</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default UserAcct;
