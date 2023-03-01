ef05d15c3a4ea40b1957b5c726ea7f882c374805
Peter Riesing <peter.riesing@gmail.com>
2023-03-01 11:19:12 -0500

    new menu item partial skeleton

--- Avocado/src/components/pages/AdminMenu.jsx
+++ Avocado/src/components/pages/AdminMenu.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import NewMenuItem from "../partials/NewMenuItem";
 
 const AdminMenu = () => {
-	return (
-		<div>
-			<form action="">
-				<h1>Add item to menu</h1>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <form action="">
+        <h1>Add item to menu</h1>
+        <NewMenuItem />
+      </form>
+    </div>
+  );
 };
 
 export default AdminMenu;
