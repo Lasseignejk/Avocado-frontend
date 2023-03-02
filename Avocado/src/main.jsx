569f83da41f55f30e54a67c293520fa5ecede8a7
Lasseignejk <jaye914@hotmail.com>
2023-03-02 14:05:59 -0500

    got get request to work

--- Avocado/src/main.jsx
+++ Avocado/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { store } from "../src/components/state/Store2";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
