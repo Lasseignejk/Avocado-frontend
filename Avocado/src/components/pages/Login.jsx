d3692ce418112ec7de6a6c2d32ce8f9297a7498f
corewill <118204159+corewill@users.noreply.github.com>
2023-03-03 17:51:52 -0500

    loginupdate

--- Avocado/src/components/pages/Login.jsx
+++ Avocado/src/components/pages/Login.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../pages/Login.css";
+import { createClient } from "@supabase/supabase-js";
+import { useDispatch, useSelector } from "react-redux";
+import { setAdmin } from "../reducers/AdminSlice";
+import { setCustomer } from "../reducers/CustomerSlice";
+const supabaseUrl = "https://dwjnomervswgqasgexck.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3am5vbWVydnN3Z3Fhc2dleGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MzEyNzAsImV4cCI6MTk5MzIwNzI3MH0.k8hjRQLV9bN_BcG11s_gWJx2NK_AHIXrJPTii7GO4LM";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const Login = () => {
+	const dispatch = useDispatch();
+	const admin = useSelector((state) => state.admin);
+	const customer = useSelector((state) => state.customer);
 	// const [accountDetails, setAccountDetails] = useState({});
 	const [loginDetails, setLoginDetails] = useState({});
 
@@ -15,20 +26,61 @@ const Login = () => {
 	};
 
 	const sendToSupabase = async (loginDetails) => {
+
+		const { CustomerEmail, Password, RestOwner } = loginDetails;
+
 		const loginBtn = document.querySelector(".loginBtn");
 		loginBtn.disabled = true;
 		loginBtn.classList.add("bg-[#b3b3b3]", "text-black");
 		loginBtn.classList.remove("bg-green", "hover:bg-blue", "text-grey");
-		const data = await fetch("http://localhost:3060/login", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(loginDetails),
-		});
-		console.log(loginDetails);
-	};
+		
+		let { data, error } = await supabase.auth.signInWithPassword({
+			email: CustomerEmail,
+			password: Password,
+			restowner: RestOwner,
 
+			
+		});
+
+		
+
+
+
+		console.log(RestOwner);
+
+		if (RestOwner == "false") {
+		  let { data, error } = await supabase.from("Customer").insert([
+			{
+			  CustomerFirstName: CustomerFirstName,
+			  CustomerLastName: CustomerLastName,
+			  CustomerEmail: CustomerEmail,
+			  CustomerPhoneNumber: CustomerPhoneNumber,
+			  Address: Address,
+			},
+		  ]);
+		  console.log(data);
+		  console.log(error);
+	
+		  const { data: user } = await supabase.auth.getUser();
+		  console.log(user);
+		  dispatch(setCustomer(user));
+		} else {
+		  let { data, error } = await supabase.from("Owner").insert([
+			{
+			  OwnerFirstName: CustomerFirstName,
+			  OwnerLastName: CustomerLastName,
+			  OwnerEmail: CustomerEmail,
+			  OwnerPhoneNumber: CustomerPhoneNumber,
+			},
+		  ]);
+		  console.log(data);
+		  console.log(error);
+	
+		  const { data: user } = await supabase.auth.getUser();
+		  console.log(user);
+		  dispatch(setAdmin(user));
+		}
+	}
 	return (
 		<div className="w-screen h-screen flex justify-center items-center bg-green">
 			<div className="w-[300px] h-[400px] bg-gray flex flex-col items-center p-5 gap-10">
