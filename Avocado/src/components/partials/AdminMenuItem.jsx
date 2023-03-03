fefc7b9457df52917e7bf7a07d62bf783e01e928
Peter Riesing <peter.riesing@gmail.com>
2023-03-03 16:35:50 -0500

    admin - menu item skeleton

---
+++ Avocado/src/components/partials/AdminMenuItem.jsx
@@ -0,0 +1,17 @@
+// card displayed when a restaurant owner clicks on a restaurant to view the menu
+
+import React from "react";
+
+const AdminMenuItem = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1>menu item name</h1>
+      <h1>menu item price</h1>
+      <button>delete menu item</button>
+      <button>update menu item</button>
+    </div>
+  );
+};
+
+export default AdminMenuItem;
