6b8ea81cf904d8b6d594e6c36feaf686b2d6478a
Peter Riesing <peter.riesing@gmail.com>
2023-03-06 14:38:56 -0500

    question marks

--- Avocado/src/components/Layout/RestaurantAdminMenuItem.jsx
+++ Avocado/src/components/Layout/RestaurantAdminMenuItem.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const RestaurantAdminMenuItem = ({ item }) => {
   return (
     <div>
-      <h1>{item.ItemName}</h1>
-      <h1>{item.ItemPrice}</h1>
-      <h1>{item.ItemType}</h1>
+      <h1>{item?.ItemName}</h1>
+      <h1>{item?.ItemPrice}</h1>
+      <h1>{item?.ItemType}</h1>
     </div>
   );
 };
