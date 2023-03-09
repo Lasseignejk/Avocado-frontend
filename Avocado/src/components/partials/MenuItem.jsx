94518c9c6151848fd48c053538d6451ff1d45607
Peter Riesing <peter.riesing@gmail.com>
2023-03-09 14:34:10 -0500

    cart work

--- Avocado/src/components/partials/MenuItem.jsx
+++ Avocado/src/components/partials/MenuItem.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import placeholder from "../../../public/items/menu_placeholder.svg";
+import { useDispatch, useSelector } from "react-redux";
+import placeholder from "/items/menu_placeholder.svg";
+import { addCart } from "../reducers/DashboardSlice";
 
 const MenuItem = ({ item }) => {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  console.log(cart);
   return (
     <div className="bg-ltgray font-niveau px-3 flex pt-3 gap-3 hover:bg-dkgray md:hover:-translate-y-1 duration-200 ease-in md:w-[400px] md:py-3">
       <div>
@@ -18,7 +23,12 @@ const MenuItem = ({ item }) => {
         <h1>{item.ItemPrice}</h1>
       </div>
       <div>
-        <button className="">Add to cart</button>
+        <button
+          className="bg-green font-bold text-gray px-2"
+          onClick={() => dispatch(addCart(item))}
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   );
