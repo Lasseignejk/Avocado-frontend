822bf9d9d14b9727f860285da03c367535ba4a60
Lasseignejk <jaye914@hotmail.com>
2023-03-09 12:33:37 -0500

    updates to admin menu view

--- Avocado/src/components/pages/Admin/Menu/RestaurantMenu.jsx
+++ Avocado/src/components/pages/Admin/Menu/RestaurantMenu.jsx
@@ -6,50 +6,85 @@ import RestaurantMenuItemCard from "./RestaurantMenuItemCard";
 import { useSelector } from "react-redux";
 
 const RestaurantMenu = () => {
-  ///previously AdminMenu
+	const restaurantId = useSelector((state) => state.currentRestaurant[0]);
+	// console.log(restaurantId);
 
-  const restaurantId = useSelector((state) => state.currentRestaurant[0]);
+	const [restToEdit, setRestToEdit] = useState({});
 
-  const [menu, setMenu] = useState();
+	useEffect(() => {
+		console.log();
+		const getRestaurant = async () => {
+			const response = await fetch(
+				import.meta.env.VITE_BACKEND + "/admin/restaurant/getonerestaurant",
+				{
+					method: "GET",
+					headers: {
+						restid: restaurantId,
+					},
+				}
+			);
 
-  useEffect(() => {
-    console.log();
-    const getMenu = async () => {
-      const response = await fetch(
-        import.meta.env.VITE_BACKEND + "/admin/restaurant/getmenu",
-        {
-          method: "GET",
-          headers: {
-            restid: restaurantId,
-          },
-        }
-      );
-      console.log(response);
-      if (!response.ok) {
-        window.alert(response.statusText);
-      } else {
-        const json = await response.json();
-        setMenu(json);
-        console.log("menu: ", json);
-      }
-    };
-    getMenu();
-  }, [restaurantId]);
+			if (!response.ok) {
+				window.alert(response?.statusText);
+			} else {
+				const json = await response?.json();
 
-  return (
-    <div className="mb-[55px] md:flex md:mb-0 justify-start">
-      <AdminNavBar />
-      <div className="">
-        <h1 className="font-bold text-center">Edit Menu</h1>
-        <div className="flex flex-col justify-evenly md:flex-row md:flex-wrap md:justify-evenly gap-3">
-          {menu?.map((item) => (
-            <RestaurantMenuItemCard setMenu={setMenu} item={item} />
-          ))}
-        </div>
-      </div>
-      <RestaurantAddMenuItemForm />
-    </div>
-  );
+				setRestToEdit(json);
+			}
+		};
+		getRestaurant();
+	}, [restaurantId]);
+
+	const [menu, setMenu] = useState();
+
+	useEffect(() => {
+		console.log();
+		const getMenu = async () => {
+			const response = await fetch(
+				import.meta.env.VITE_BACKEND + "/admin/restaurant/getmenu",
+				{
+					method: "GET",
+					headers: {
+						restid: restaurantId,
+					},
+				}
+			);
+			if (!response.ok) {
+				window.alert(response.statusText);
+			} else {
+				const json = await response.json();
+				setMenu(json);
+				// console.log("menu: ", json);
+			}
+		};
+		getMenu();
+	}, [restaurantId]);
+
+	return (
+		<div className="mb-[55px] md:flex md:mb-0 justify-start">
+			<AdminNavBar />
+			<div className="flex flex-col gap-10 pt-3 md:w-full md:px-16 md:pt-20 md:flex-row">
+				<div>
+					<div className="flex flex-col gap-3">
+						<h1 className="text-center text-4xl font-bold text-green md:text-left">
+							{restToEdit[0]?.RestName}
+						</h1>
+						<h1 className="text-center text-3xl font-bold md:text-left">
+							Edit Menu
+						</h1>
+					</div>
+					<div className="flex flex-col justify-evenly md:flex-row md:flex-wrap md:justify-evenly gap-3">
+						{menu?.map((item) => (
+							<RestaurantMenuItemCard setMenu={setMenu} item={item} />
+						))}
+					</div>
+				</div>
+				<div className="flex justify-center">
+					<RestaurantAddMenuItemForm />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default RestaurantMenu;
