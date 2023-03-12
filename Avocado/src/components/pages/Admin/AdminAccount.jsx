a40051aaf43bf300cbfa01fd5c573f4ad9cf4bd2
Lasseignejk <jaye914@hotmail.com>
2023-03-11 20:50:30 -0500

    CSS/state fixes to admin account, other pages

--- Avocado/src/components/pages/Admin/AdminAccount.jsx
+++ Avocado/src/components/pages/Admin/AdminAccount.jsx
@@ -1,12 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import AdminNavBar from "../../partials/AdminNavBar";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DeleteAdminForm from "./DeleteAdminForm";
 
 const AdminAccount = () => {
 	const userDetails = useSelector((state) => state?.userDetails[0]);
 	const dispatch = useDispatch();
 
 	const [updateDetails, setupdateDetails] = useState({});
+	const [rest, setRest] = useState([{}]);
+	const [toggle, setToggle] = useState(true);
+
+	const getRestaurants = async () => {
+		const response = await fetch(
+			import.meta.env.VITE_BACKEND + "/admin/getrestaurants",
+
+			{
+				method: "GET",
+				headers: {
+					userid: userDetails.id,
+				},
+			}
+		);
+		if (!response.ok) {
+			window.alert(response.statusText);
+		} else {
+			const data = await response.json();
+			setRest(data);
+		}
+	};
+
+	useEffect(() => {
+		getRestaurants();
+	}, [toggle]);
 
 	const setFormState = (e) => {
 		setupdateDetails({
@@ -18,38 +46,55 @@ const AdminAccount = () => {
 
 	const updateUserAccount = async (updateDetails) => {
 		console.log(updateDetails);
-		const data = await fetch("http://localhost:3060/admin/updateAdmin", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(updateDetails),
-		});
+		const data = await fetch(
+			import.meta.env.VITE_BACKEND + "/admin/updateAdmin",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(updateDetails),
+			}
+		);
+		if (data) {
+			setToggle(!toggle);
+			console.log(toggle);
+			setupdateDetails({});
+		}
 	};
 
 	return (
-		<div className="mb-[55px] md:flex md:mb-0">
+		<div className="mb-[55px] lg:flex lg:mb-0">
 			<AdminNavBar />
-			<div className="flex flex-col gap-10 pt-3 md:w-full md:px-16 md:pt-20">
+			<div className="flex flex-col gap-10 pt-3 lg:w-full lg:px-16 lg:pt-10">
+				<ToastContainer />
 				<div className="flex flex-col gap-3">
-					<h1 className="text-center text-4xl font-bold text-green md:text-left">
-						{userDetails?.OwnerFirstName}'s Account Details
+					<h1 className="text-center text-4xl px-3 lg:px-0 font-bold text-green lg:text-left">
+						{userDetails?.OwnerFirstName} {userDetails?.OwnerLastName}
+					</h1>
+					<h1 className="text-center text-3xl font-bold lg:text-left">
+						Account Details
 					</h1>
 				</div>
-				<div className="flex justify-center">
-					<form className="px-3 flex flex-col gap-3 mb-3 items-center w-full md:w-1/2 md:mb-5">
-						<h1 className="text-2xl font-bold text-center">Update Account</h1>
+
+				<div className="flex justify-center lg:justify-start">
+					<form className="px-3 py-5 flex flex-col gap-3 mb-3 items-center w-[80%] md:w-1/2 md:mb-5 bg-ltgray rounded-xl shadow-lg lg:w-1/3">
+						<h1 className="text-2xl font-bold text-center tracking-wider">
+							Update Account
+						</h1>
 
 						<div className="flex flex-col w-full">
-							<label className="font-bold" htmlFor="firstName">
+							<label
+								className="font-bold text-xl tracking-wide"
+								htmlFor="firstName">
 								First Name
 							</label>
 							<input
-								className="pl-3"
+								className="pl-1 py-1 border-b-2 border-black bg-ltgray font-thin"
 								type="text"
 								id="firstName"
 								name="OwnerFirstName"
-								placeholder={userDetails.OwnerFirstName}
+								placeholder={userDetails?.OwnerFirstName}
 								onChange={(e) => setFormState(e)}
 								value={
 									updateDetails.OwnerFirstName
@@ -59,15 +104,17 @@ const AdminAccount = () => {
 							/>
 						</div>
 						<div className="flex flex-col w-full">
-							<label className="font-bold" htmlFor="lastName">
+							<label
+								className="font-bold text-xl tracking-wide"
+								htmlFor="lastName">
 								Last Name
 							</label>
 							<input
-								className="pl-3"
+								className="pl-1 py-1 border-b-2 border-black bg-ltgray font-thin"
 								type="text"
 								id="lastName"
 								name="OwnerLastName"
-								placeholder={userDetails.OwnerLastName}
+								placeholder={userDetails?.OwnerLastName}
 								onChange={(e) => setFormState(e)}
 								value={
 									updateDetails.OwnerLastName ? updateDetails.OwnerLastName : ""
@@ -76,15 +123,17 @@ const AdminAccount = () => {
 						</div>
 
 						<div className="flex flex-col w-full">
-							<label className="font-bold" htmlFor="phone">
+							<label
+								className="font-bold text-xl tracking-wide"
+								htmlFor="phone">
 								Phone Number
 							</label>
 							<input
-								className="pl-3"
+								className="pl-1 py-1 border-b-2 border-black bg-ltgray font-thin"
 								type="text"
 								id="phone"
 								name="OwnerPhoneNumber"
-								placeholder={userDetails.OwnerPhoneNumber}
+								placeholder={userDetails?.OwnerPhoneNumber}
 								onChange={(e) => setFormState(e)}
 								value={
 									updateDetails.OwnerPhoneNumber
@@ -96,14 +145,15 @@ const AdminAccount = () => {
 
 						<div className="flex justify-center">
 							<button
-								className="bg-green text-gray text-lg px-5 py-1 duration-200 font-bold hover:bg-dkgreen"
+								className="bg-green text-gray text-lg px-3 py-1 duration-200 font-bold tracking-widest hover:bg-dkgreen rounded-full shadow-lg ease-in"
 								type="button"
 								onClick={() => updateUserAccount(updateDetails)}>
-								Submit Update
+								UPDATE
 							</button>
 						</div>
 					</form>
 				</div>
+				<DeleteAdminForm rest={rest} setRest={setRest} />
 			</div>
 		</div>
 	);
--- Avocado/src/components/partials/AdminNavBarLinks.jsx
+++ Avocado/src/components/partials/AdminNavBarLinks.jsx
@@ -5,7 +5,7 @@ const AdminNavBarLinks = ({ links }) => {
 	return (
 		<>
 			{links.map((link, index) => (
-				<li className="border-b-2 border-gray pb-5 h-[50px]" index={index}>
+				<li className="border-b-2 border-gray pb-5 h-[50px]" key={index}>
 					<Link
 						to={link.path}
 						className=" flex justify-center w-full hover:bg-dkgreen hover:text-gray duration-200 ease-in rounded-full lg:px-3 lg:py-1">
