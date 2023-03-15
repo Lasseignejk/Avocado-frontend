2c130dda656ff95fd96e2ed24da79515d6c9b586
Peter Riesing <peter.riesing@gmail.com>
2023-03-14 20:21:16 -0400

    finished image / page

--- Avocado/src/components/pages/Customer/OrderComplete.jsx
+++ Avocado/src/components/pages/Customer/OrderComplete.jsx
@@ -3,9 +3,13 @@ import CustomerNavBar from "../../partials/CustomerNavBar";
 
 const OrderComplete = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col justify-center gap-4">
       <CustomerNavBar />
-      <h1>Order Complete!</h1>
+      <img
+        className="mx-auto my-10 lg:h-screen md:w-[50%] sm:w-[80%]"
+        src="/items/finished.svg"
+        alt="ordercomplete"
+      />
     </div>
   );
 };
